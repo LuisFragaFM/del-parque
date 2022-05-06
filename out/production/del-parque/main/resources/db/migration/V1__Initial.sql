2dd5a8f6e33c8db85376283e5bbfd9f8efcc3e6e
JEdilbertoTC <joseedilberto367@outlook.com>
2022-05-06 09:46:01 -0500

    agregar nuevos endpoints

---
+++ out/production/del-parque/main/resources/db/migration/V1__Initial.sql
@@ -0,0 +1,102 @@
+CREATE TABLE calles
+(
+    id     varchar(40) primary key,
+    nombre varchar(40)
+);
+
+CREATE TABLE direcciones
+(
+    id              varchar(40) primary key,
+    numero_casa     varchar(10),
+    id_calle        varchar(40) references calles,
+    nombre_colonia  varchar(40),
+    codigo_postal   varchar(10),
+    numero_interior varchar(10)
+);
+
+CREATE TABLE contactos
+(
+    id                  varchar(40) primary key,
+    email               varchar(120),
+    numero_telefono     varchar(20),
+    telefono_emergencia varchar(20)
+);
+
+CREATE TABLE roles
+(
+    id            varchar(40) primary key,
+    administrador boolean,
+    vigilante     boolean,
+    residente     boolean
+);
+
+CREATE TABLE usuarios
+(
+    id          varchar(40) primary key,
+    id_roles    varchar(40) references roles,
+    id_contacto varchar(40) references contactos,
+    nombre      varchar(120)
+);
+
+CREATE TABLE automoviles
+(
+    id      varchar(40) primary key,
+    placas  varchar(40),
+    color   varchar(40),
+    modelo  varchar(40),
+    tarjeta varchar(40)
+);
+
+CREATE TABLE trabajadores
+(
+    id     varchar(40) primary key,
+    nombre varchar(120),
+    fecha  date not null,
+    caseta varchar(40),
+    hora   date not null
+);
+
+CREATE TABLE condominios
+(
+    id            varchar(40) primary key,
+    id_usuario    varchar(40) references usuarios,
+    id_direccion  varchar(40) references direcciones,
+    id_trabajador varchar(40) references trabajadores
+);
+
+CREATE TABLE visitantes
+(
+    id                 varchar(40) primary key,
+    id_automovil       varchar(40) references automoviles,
+    id_condominio      varchar(40) references condominios,
+    nombre             varchar(40),
+    fecha              date not null,
+    vigilante_autorizo varchar(120),
+    residente          varchar(120),
+    caseta_entrada     varchar(40),
+    caseta_salida      varchar(40)
+);
+
+CREATE TABLE paquetes
+(
+    id            varchar(40) primary key,
+    emisor        varchar(120),
+    receptor      varchar(120),
+    caseta        varchar(120),
+    entregado     boolean,
+    id_condominio varchar(40) references condominios
+);
+
+CREATE TABLE entregas
+(
+    id            varchar(40) primary key,
+    quien_entrega varchar(120),
+    id_paquete    varchar(40) references paquetes
+);
+
+CREATE TABLE pagos
+(
+    id            varchar(40) primary key,
+    pagado        boolean,
+    id_condominio varchar(40) references condominios
+);
