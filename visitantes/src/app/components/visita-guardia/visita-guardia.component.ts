a4c8be036f3ef0bc9b45b28e93dd590723fea242
Chemaiss <a299379@alumnos.uaslp.mx>
2022-08-16 15:52:46 -0500

    agregado guardado de visitantes guardia/admin

--- visitantes/src/app/components/visita-guardia/visita-guardia.component.ts
+++ visitantes/src/app/components/visita-guardia/visita-guardia.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {VisitantesService} from "../../services/visitantes.service";
+import {Visitante} from "../../models/visitante";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-visita-guardia',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visita-guardia.component.css']
 })
 export class VisitaGuardiaComponent implements OnInit {
-
-  constructor() { }
+  visitante: Visitante = {} as Visitante;
+  constructor(private visitantesService: VisitantesService) { }
 
   ngOnInit(): void {
+  }
+
+  save() {
+    this.visitantesService.save(this.visitante).subscribe(visitante => {
+      Swal.fire({
+        title: `La visita de ${visitante.nombreVisitante} fue agendada`,
+        icon: 'success',
+        showDenyButton: false,
+        showCancelButton: false,
+        confirmButtonText: `Cerrar`
+      }).then(() => {});
+    });
   }
 
 }
