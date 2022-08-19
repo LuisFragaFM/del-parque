7dd963b22962d8d18bbfdea11a43d92611e87324
GabrielVillagran <metalmadnes98@gmail.com>
2022-08-18 23:45:04 -0500

    se agregaron los componentes y modulos necesarios

--- visitantes/src/app/components/entregar-paquete/entregar-paquete.component.ts
+++ visitantes/src/app/components/entregar-paquete/entregar-paquete.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+// modelos, servicios y alertas
+import {Paquete} from "../../models/paquete"; //Modelo de paquete
+import {PaquetesService} from "../../services/paquetes.service"; //Servicio de paquete
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-entregar-paquete',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entregar-paquete.component.css']
 })
 export class EntregarPaqueteComponent implements OnInit {
-
-  constructor() { }
-
+  Entrega_paquete: Paquete = {} as Paquete;
+  constructor(private PaquetesService: PaquetesService) {
+  }
   ngOnInit(): void {
   }
 
+  // Notificaciones de usuario
+  save() {
+    this.PaquetesService.save(this.Entrega_paquete).subscribe(Entrega_paquete => {
+      this. Entrega_paquete = {} as Paquete;
+      Swal.fire({
+        title: `El paquete fue recibido por ${Entrega_paquete.recibeGuardia} y guardado en ${Entrega_paquete.casetaEntrega}`,
+        icon: 'success',
+        showDenyButton: false,
+        showCancelButton: false,
+        confirmButtonText: `Cerrar`
+      }).then(() => {
+        this.Entrega_paquete = {} as Paquete;
+
+      });
+    });
+  }
 }
