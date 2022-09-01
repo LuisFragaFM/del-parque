e84b5fa4940756e358a7f7871ae8bf75a8ef5775
JEdilbertoTC <joseedilberto367@outlook.com>
2022-09-01 15:42:28 -0500

    roles

--- backend/src/main/java/com/example/delparque/controllers/VisitantesController.java
+++ backend/src/main/java/com/example/delparque/controllers/VisitantesController.java
@@ -18,25 +18,25 @@ public class VisitantesController {
     }
 
     @GetMapping()
-    @PreAuthorize("hasAnyRole('ROLE_GUARD')")
+    @PreAuthorize("hasAnyRole('ROLE_GUARD', 'ROLE_ADMIN')")
     public Page<Visitante> findAll(@RequestParam Integer page) {
         return visitantesService.findAll(page);
     }
 
     @GetMapping("{id}")
-    @PreAuthorize("hasAnyRole('ROLE_GUARD')")
+    @PreAuthorize("hasAnyRole('ROLE_GUARD', 'ROLE_ADMIN')")
     public Visitante findById(@PathVariable String id) {
         return visitantesService.findById(id);
     }
 
     @PostMapping()
-    @PreAuthorize("hasAnyRole('ROLE_GUARD', 'ROLE_RESIDENT')")
+    @PreAuthorize("hasAnyRole('ROLE_GUARD', 'ROLE_RESIDENT', 'ROLE_ADMIN')")
     public Visitante save(@RequestBody Visitante visitante) {
         return visitantesService.save(visitante);
     }
 
     @DeleteMapping("{id}")
-    @PreAuthorize("hasAnyRole('ROLE_GUARD', 'ROLE_RESIDENT')")
+    @PreAuthorize("hasAnyRole('ROLE_GUARD', 'ROLE_RESIDENT', 'ROLE_ADMIN')")
     public void delete(@PathVariable String id) {
         visitantesService.delete(id);
     }
