cd411453676bc8cf39f54dafea2cf9d89a791360
JEdilbertoTC <joseedilberto367@outlook.com>
2022-09-01 15:33:39 -0500

    roles

--- backend/src/main/java/com/example/delparque/controllers/CondominosController.java
+++ backend/src/main/java/com/example/delparque/controllers/CondominosController.java
@@ -3,6 +3,7 @@ package com.example.delparque.controllers;
 import com.example.delparque.dto.Condomino;
 import com.example.delparque.service.CondominosService;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -17,31 +18,37 @@ public class CondominosController {
     }
 
     @GetMapping()
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public Page<Condomino> findAll(@RequestParam Integer page) {
         return condominosService.findAll(page);
     }
 
     @GetMapping("{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public Condomino findById(@PathVariable String id) {
         return condominosService.findById(id);
     }
 
     @GetMapping("nombre")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public Condomino findByNombre(@RequestParam() String nombre) {
         return condominosService.findByNombre(nombre);
     }
 
     @GetMapping("telefono")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public Condomino findByNumeroTelefono(@RequestParam() String telefono) {
         return condominosService.findByNumeroTelefono(telefono);
     }
 
     @PostMapping()
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public Condomino save(@RequestBody Condomino condomino) {
         return condominosService.save(condomino);
     }
 
     @DeleteMapping("{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public void delete(@PathVariable String id) {
         condominosService.delete(id);
     }
--- backend/src/main/java/com/example/delparque/controllers/UsuariosController.java
+++ backend/src/main/java/com/example/delparque/controllers/UsuariosController.java
@@ -3,7 +3,6 @@ package com.example.delparque.controllers;
 import com.example.delparque.dto.Usuario;
 import com.example.delparque.service.UsuariosService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
