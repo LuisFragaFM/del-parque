3bec313d3306df031ff187cd78997a5c67c56ec6
Immortaldoor001 <urieljrzmtz99@gmail.com>
2022-09-19 18:27:41 -0500

    Paginacion de registro de visitas agendadas

--- visitantes/src/app/components/agenda-condomino/agenda-condomino.component.ts
+++ visitantes/src/app/components/agenda-condomino/agenda-condomino.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {VisitantesService} from "../../services/visitantes.service";
+import {Visitante} from "../../models/visitante";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-agenda-condomino',
@@ -6,10 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agenda-condomino.component.css']
 })
 export class AgendaCondominoComponent implements OnInit {
-
-  constructor() { }
+  
+  // datos usados para realizar la busqueda
+  name: string | undefined;
+  visitante: Visitante = {} as Visitante;
+  visitantes: Visitante[] = [];
+  page: number = 0;
+  listOfPages: number[] = [];
+  constructor(private visitantesService: VisitantesService) { }
 
   ngOnInit(): void {
+    //Paginacion
+    this.visitantesService.getVisitantes(1).subscribe((visitantes)=>{
+
+      this.visitantes = visitantes.content;
+    });
+    this.visitantesService.getVisitantes(this.page).subscribe(visitantes => {
+      this.visitantes = visitantes.content;
+      const totalOfPages = 10 //Math.trunc(trabajadores.totalElements / trabajadores.size);
+      for (let i = 0; i < totalOfPages; i++) {
+        this.listOfPages.push(i + 1);
+      }
+    })
+  }
+
+  // busqueda de visitante
+  findVisitante() {
+    if (this.name) {
+      this.visitantesService.findByName(this.name).subscribe(visitante => {
+        console.log(visitante);
+        this.visitante = visitante;
+      });
+    } else {
+      this.visitantesService.findByName(this.name!).subscribe(visitante => {
+        this.visitante = visitante;
+      });
+    }
+
+  }
+  
+  updatePage(page: number) {
+    this.page = page - 1;
+    this.visitantesService.getVisitantes(this.page).subscribe(visitantes => {
+      this.visitantes = visitantes.content;
+    })
   }
 
 }
