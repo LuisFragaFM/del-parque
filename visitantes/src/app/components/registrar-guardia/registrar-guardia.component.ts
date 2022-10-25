2bf69d1bf75092806b79a551e9f9a5e3dec7ad94
¨Chemaiss <a299379@alumnos.uaslp.mx>
2022-10-25 18:34:14 -0500

    generado componente registro guardias

---
+++ visitantes/src/app/components/registrar-guardia/registrar-guardia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-guardia',
+  templateUrl: './registrar-guardia.component.html',
+  styleUrls: ['./registrar-guardia.component.css']
+})
+export class RegistrarGuardiaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
