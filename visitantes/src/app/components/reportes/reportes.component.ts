f408938de22e78c2bccb5dac79ead49c3073360e
¨Chemaiss <a299379@alumnos.uaslp.mx>
2022-11-08 22:17:50 -0600

    continuacion

--- visitantes/src/app/components/reportes/reportes.component.ts
+++ visitantes/src/app/components/reportes/reportes.component.ts
@@ -2,6 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {CondominosService} from "../../services/condominos.service";
 import {Condomino} from "../../models/condomino";
 import {ReporteService} from "../../services/reporte.service";
+import {UploadFilesService} from "../../services/upload-files.service";
+import {environment} from "../../../environments/environment";
+import {UserView} from "../../models/userview";
+import {UsuariosService} from "../../services/usuarios.service";
 
 @Component({
   selector: 'app-reportes',
@@ -14,11 +18,19 @@ export class ReportesComponent implements OnInit {
   phone: string | undefined;
   condomino: Condomino = {} as Condomino;
   condominos: Condomino[] = [];
-  constructor(private condominosService: CondominosService, private reporteService: ReporteService) {
+  image: any;
+  environment = environment.baseUrl;
+  uri: any = '';
+  files!: FileList;
+  constructor(private condominosService: CondominosService, 
+    private reporteService: ReporteService,
+    private uploadFilesService: UploadFilesService,
+    private usuariosService: UsuariosService) {
   }
   ngOnInit(): void {
+    this.condomino.user = {} as UserView;
   }
-  findInquilino() {
+  /*findInquilino() {
 
 
     if (this.phone) {
@@ -31,6 +43,32 @@ export class ReportesComponent implements OnInit {
       this.condominosService.findByName(this.name!).subscribe(condominos => {
         this.condominos = condominos;
       });
+    }
+  }*/
+  findInquilinoByName() {
+    this.condominosService.findByName(this.name!).subscribe(condominos => {
+      this.condominos = condominos;
+    });
+  }
+
+  selectInquilino(condomino: Condomino) {
+    this.condomino = condomino;
+    this.condominos = [];
+    this.name = undefined;
+    this.uploadFilesService.loadFilename(this.condomino.user.id).subscribe(({filename}) => {
+      this.uri = this.environment + '/file/' + filename;
+    });
+  }
+
+  changeImage(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    this.files = target.files as FileList;
+
+    const reader = new FileReader();
+    reader.readAsDataURL(this.files[0]);
+
+    reader.onload = (_event) => {
+      this.uri = reader.result;
     }
   }
 
